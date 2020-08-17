13cd8c346ace6f91363a1aa815bb80f21cd1aefb
AliSafari-IT <r0803103@student.thomasmore.be>
2020-08-17 23:33:00 +0200

    first commit

---
+++ party/src/main/java/thomasmore/be/party/PartyApplication.java
@@ -0,0 +1,13 @@
+package thomasmore.be.party;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PartyApplication.class, args);
+    }
+
+}
---
+++ party/src/test/java/thomasmore/be/party/PartyApplicationTests.java
@@ -0,0 +1,13 @@
+package thomasmore.be.party;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
